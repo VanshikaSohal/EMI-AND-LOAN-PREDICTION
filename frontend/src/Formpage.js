e8ed5b45efa44e32cd9e39b9ea57501729f4d016
Vanshika Sohal <vanshika07sohal@gmail.com>
2025-07-25 10:21:08 +0530

    hello

--- frontend/src/Formpage.js
+++ frontend/src/Formpage.js
@@ -32,7 +32,7 @@ function Formpage({ setResult, result, setPage }) {
   }
 
   try {    
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch(" https://emi-and-loan-prediction-4qva.onrender.com/predict", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
